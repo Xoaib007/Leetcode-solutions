c210424628a8ffd59972bc62f1073a65fb09ffc3
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-01-27 22:42:03 +0100

    Update and rename 896.MonotonicArray.java to 896.java

--- Easy/896.java
+++ Easy/896.java
@@ -1,4 +1,8 @@
 /******************************************************************************
+
+Monotonic Array
+---------------
+
 An array is monotonic if it is either monotone increasing or monotone decreasing.
 
 An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
