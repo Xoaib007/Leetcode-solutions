5de9dc431f1f2082a92f2f799bb1e8188d480ce5
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-02-25 08:34:22 +0100

    Update 0001.java

--- Easy/Solutions/0001.java
+++ Easy/Solutions/0001.java
@@ -1,41 +1,22 @@
-/******************************************************************************
-Two Sum
--------
-
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-
-Example 1:
----------
-
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
-Example 2:
-----------
-
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-Example 3:
-
-Input: nums = [3,3], target = 6
-Output: [0,1]
-
-*******************************************************************************/
-
 class Solution {
+    /**
+     * Finds indices of two numbers that add up to the target.
+     *
+     * @param nums   Array of integers.
+     * @param target Target sum to find.
+     * @return       Indices of the two numbers.
+     */
     public int[] twoSum(int[] nums, int target) {
+        // Iterate through each element in the array
         for (int i = 0; i < nums.length - 1; i++) {
-        for (int j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                return new int[] {i, j};
+            // Check for a pair that sums to the target
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j}; // Return indices of the two numbers
+                }
             }
         }
-    }
-    throw new IllegalArgumentException("No two sum solution");
+        // Throw an exception if no valid pair is found
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
