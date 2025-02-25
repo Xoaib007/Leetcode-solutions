680118a09e591db5caac1adbd5e1a93fc7aee494
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-02-25 08:40:37 +0100

    Update 0013.java

--- Easy/Solutions/0013.java
+++ Easy/Solutions/0013.java
@@ -1,32 +1,14 @@
-/******************************************************************************
-RomantoInteger
---------------
-
-Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
-
-Symbol       Value
-I             1
-V             5
-X             10
-L             50
-C             100
-D             500
-M             1000
-For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
-
-Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
-
-I can be placed before V (5) and X (10) to make 4 and 9. 
-X can be placed before L (50) and C (100) to make 40 and 90. 
-C can be placed before D (500) and M (1000) to make 400 and 900.
-Given a roman numeral, convert it to an integer.
-
-*******************************************************************************/
-
 import java.util.*;
 
 class Solution {
+    /**
+     * Converts a Roman numeral string to an integer.
+     *
+     * @param s The Roman numeral string.
+     * @return  The integer representation of the Roman numeral.
+     */
     public int romanToInt(String s) {
+        // Map to store Roman numeral values
         Map<Character, Integer> romanMap = new HashMap<>();
         romanMap.put('I', 1);
         romanMap.put('V', 5);
@@ -39,13 +21,17 @@ class Solution {
         int result = 0;
         int prevValue = 0;
 
+        // Iterate through the string from right to left
         for (int i = s.length() - 1; i >= 0; i--) {
             int currValue = romanMap.get(s.charAt(i));
+            
+            // If current value is smaller than previous, subtract it; otherwise, add it
             if (currValue < prevValue) {
                 result -= currValue;
             } else {
                 result += currValue;
             }
+            
             prevValue = currValue;
         }
 
