315ef164f9f1742b016c9c46dfea8ea742870b92
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-02-25 08:42:34 +0100

    Update 0027.java

--- Easy/Solutions/0027.java
+++ Easy/Solutions/0027.java
@@ -1,10 +1,18 @@
 class Solution {
+    /**
+     * Removes all occurrences of a given value in-place.
+     *
+     * @param nums The integer array.
+     * @param val  The value to remove.
+     * @return     The new length of the modified array.
+     */
     public int removeElement(int[] nums, int val) {
-         int k = 0; // Position to place the non-val elements
+        int k = 0; // Position to place the non-val elements
 
+        // Iterate through the array
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i];
+            if (nums[i] != val) { // If element is not the target value
+                nums[k] = nums[i]; // Move it forward
                 k++;
             }
         }
