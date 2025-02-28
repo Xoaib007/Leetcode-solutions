030ebf9a4c25371b3b9b78156b47cc02d2a6461c
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-02-28 11:42:48 +0100

    Update 0657.java

--- Easy/Solutions/0657.java
+++ Easy/Solutions/0657.java
@@ -1,22 +1,31 @@
 class Solution {
+    /**
+     * Determines whether a sequence of moves returns a robot to its starting position.
+     * 
+     * The robot moves based on:
+     * - 'R' -> Moves right (x +1)
+     * - 'L' -> Moves left (x -1)
+     * - 'U' -> Moves up (y +1)
+     * - 'D' -> Moves down (y -1)
+     *
+     * @param moves A string representing movement instructions ('R', 'L', 'U', 'D').
+     * @return      True if the robot returns to the origin, otherwise false.
+     */
     public boolean judgeCircle(String moves) {
-        int x = 0;
-        int y = 0;
+        int x = 0; // Tracks horizontal movement
+        int y = 0; // Tracks vertical movement
 
-        char[] movesArray = moves.toCharArray();
+        char[] movesArray = moves.toCharArray(); // Convert string to character array
 
         // Iterate through the character array to update coordinates
-        for(int i = 0; i < movesArray.length; i++){
-            if(movesArray[i] == 'R'){
+        for (char move : movesArray) {
+            if (move == 'R') {
                 x += 1; // Move right increases x
-            }
-            if(movesArray[i] == 'L'){
+            } else if (move == 'L') {
                 x -= 1; // Move left decreases x
-            }
-            if(movesArray[i] == 'U'){
+            } else if (move == 'U') {
                 y += 1; // Move up increases y
-            }
-            if(movesArray[i] == 'D'){
+            } else if (move == 'D') {
                 y -= 1; // Move down decreases y
             }
         }
