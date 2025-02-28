0007bd69516f0a743b123840a5990e01cf9faa9b
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-02-28 11:43:44 +0100

    Update 0896.java

--- Easy/Solutions/0896.java
+++ Easy/Solutions/0896.java
@@ -1,44 +1,27 @@
-/******************************************************************************
-
-Monotonic Array
----------------
-
-An array is monotonic if it is either monotone increasing or monotone decreasing.
-
-An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
-
-Given an integer array nums, return true if the given array is monotonic, or false otherwise.
-
- 
-
-Example 1:
-
-Input: nums = [1,2,2,3]
-Output: true
-Example 2:
-
-Input: nums = [6,5,4,4]
-Output: true
-Example 3:
-
-Input: nums = [1,3,2]
-Output: false
- 
-*******************************************************************************/
-
 class Solution {
+    /**
+     * Determines if an array is monotonic (either entirely non-increasing or non-decreasing).
+     * 
+     * - A sequence is increasing if each element is >= the previous one.
+     * - A sequence is decreasing if each element is <= the previous one.
+     * 
+     * @param nums The integer array to check.
+     * @return     True if the array is monotonic, otherwise false.
+     */
     public boolean isMonotonic(int[] nums) {
-        boolean increasing = true;
-        boolean decreasing = true;
+        boolean increasing = true;  // Assume array is non-decreasing
+        boolean decreasing = true;  // Assume array is non-increasing
 
+        // Iterate through the array and check the trend
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] > nums[i - 1]) {
-                decreasing = false;
+                decreasing = false; // If an increase is found, it's not non-increasing
             } else if (nums[i] < nums[i - 1]) {
-                increasing = false;
+                increasing = false; // If a decrease is found, it's not non-decreasing
             }
         }
 
+        // The array is monotonic if it's either entirely non-decreasing or non-increasing
         return increasing || decreasing;
     }
 }
