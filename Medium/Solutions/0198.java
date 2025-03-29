421b918b9c97d0a030f3e0d2994da8ac404e334b
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-03-29 03:50:20 +0100

    Update 0198.java

--- Medium/Solutions/0198.java
+++ Medium/Solutions/0198.java
@@ -1,46 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-        int money = 0;
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
 
-        if (nums.length == 2) {
-            if (nums[0] > nums[1]) {
-                return nums[0];
+        int robbedSet1 = 0; // Start from house 0
+        int robbedSet2 = 0; // Start from house 1
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                robbedSet1 += nums[i]; // Pick even index houses
             } else {
-                return nums[1];
+                robbedSet2 += nums[i]; // Pick odd index houses
             }
         }
 
-        if (nums.length == 3) {
-            if (nums[0] + nums[2] > nums[1]) {
-                return nums[0] + nums[2];
-            } else {
-                return nums[1];
-            }
-        }
-
-        int totalHouse = nums.length;
-        int houseIndex1 = 0;
-
-        int robbedSet1 = 0;
-        while (houseIndex1 <= totalHouse - 1) {
-            robbedSet1 += nums[houseIndex1];
-            houseIndex1 += 2;
-        }
-
-         int houseIndex2 = 1;
-
-        int robbedSet2 = 0;
-        while (houseIndex2 <= totalHouse - 1) {
-            robbedSet2 += nums[houseIndex2];
-            houseIndex2 += 2;
-        }
-
-        if(robbedSet1 < robbedSet2) {
-       money = robbedSet2;
-    }else {
-       money = robbedSet1;
-    }
-
-        return money;
+        return Math.max(robbedSet1, robbedSet2); // Return max of both sets
     }
 }
